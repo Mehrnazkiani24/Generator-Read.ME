6977aa39f81b4ccc987793d9ad387edb60c5e6de
mehrnazkiani24 <mehrnaz03@yahoo.com>
2024-04-12 13:56:48 -0700

    added nodemodules

---
+++ index.js
@@ -0,0 +1,52 @@
+const inquirer = require("inquirer")
+
+inquirer.prompt([
+    {
+        type:"input",
+        name:"title",
+        message:"Enter Project Title: "
+    },
+    {
+        type:"input",
+        name:"Discription",
+        message:"Enter Project Discriptiom: "
+    },
+    {
+        type:"input",
+        name:"installation",
+        message:"Enter Project installation: "
+    },
+    {
+        type:"input",
+        name:"testing",
+        message:"Enter Project testing: "
+    },
+    {
+        type:"input",
+        name:"contribution",
+        message:"Enter Project contribution: "
+    },
+    {
+        type:"input",
+        name:"usage",
+        message:"Enter Project usage: "
+    },
+    {
+        type:"input",
+        name:"github",
+        message:"Enter Project github: "
+    },
+    {
+        type:"input",
+        name:"email",
+        message:"Enter Project email: "
+    },
+    {
+        type:"list",
+        name:"license",
+        message:"Enter Project license: ",
+        choices:["MIT","ISC","APACHE 2.0","None"]
+    },
+]).then(response =>{
+    console.log(response);
+})
